e6f855d9469716a6eaea21ac50e24a5ecfc7edff
Steven Basart <xksteven@users.noreply.github.com>
2024-06-19 02:32:49 -0400

    Remove unused parameter now that we're reading from HF
    WIP Patch for qwq eval
    
    WIP fixed paths
    
    Remove unused parameter now that we're reading from HF
    
    Squash commit

--- eval/sbatch/submit_all_jobs.sh
+++ eval/sbatch/submit_all_jobs.sh
@@ -1,14 +1,19 @@
-APPS_EVAL_DIR="~/apps-beta/min_eval/"
+APPS_EVAL_DIR="../" # path to apps/eval
 SKIP_AMT=20
-SAVE_LOC="~/apps-beta/min_eval/results"
-MODEL_LOC='~/apps-beta/modelling/checkpoints/final_checkpoint/'
-TEST_LOC="~/apps-beta/data_split/test.json"
-TOTAL_PROBLEMS=10640
+SAVE_LOC="../../../results" # path to results dir
+MODEL_LOC=':)' # basicly anything non-empty it does not matter but if old code is used -- path to model
+TEST_LOC="../../../APPS/test" # path to test data
+TOTAL_PROBLEMS=20
+
+export HF_HOME=/mnt/LLM
+export CUDA_VISIBLE_DEVICES=3
 
 for (( i=0; i <= $TOTAL_PROBLEMS ; i+=$SKIP_AMT)) ; 
 do 
-    echo "$frac $i"
-    jid1=$(sbatch --parsable start_slurm_gen.sbatch $APPS_EVAL_DIR $i $(($i+$SKIP_AMT)) $SAVE_LOC  $MODEL_LOC  $TEST_LOC )
-    jid2=$(sbatch --dependency=afterany:$jid1 test_all_sols.sbatch $APPS_EVAL_DIR $i $(($i+$SKIP_AMT)) $SAVE_LOC  $TEST_LOC )
+    echo "It: $i - $($i + $SKIP_AMT), cuda: $CUDA_VISIBLE_DEVICES, HF_HOME: $HF_HOME"
+    # jid1=$(sbatch --parsable start_slurm_gen.sbatch $APPS_EVAL_DIR $i $(($i+$SKIP_AMT)) $SAVE_LOC  $MODEL_LOC  $TEST_LOC )
+    bash start_slurm_gen.sbatch $APPS_EVAL_DIR $i $(($i+$SKIP_AMT)) $SAVE_LOC  $MODEL_LOC  $TEST_LOC
+    # jid2=$(sbatch --dependency=afterany:$jid1 test_all_sols.sbatch $APPS_EVAL_DIR $i $(($i+$SKIP_AMT)) $SAVE_LOC  $TEST_LOC )
+    bash test_all_sols.sbatch $APPS_EVAL_DIR $i $(($i+$SKIP_AMT)) $SAVE_LOC  $TEST_LOC
 done
 
